9122929b43482a059a0e95195b3f325eeec40721
krypto <krypt0pr0xy@mail.ch>
2020-11-06 15:59:31 +0100

    Updated Software

--- Streufeldkompensation_SPI_V1/main.c
+++ Streufeldkompensation_SPI_V1/main.c
@@ -1,13 +1,16 @@
-#include <msp430.h> 
-
-
-/**
- * main.c
+/*
+ * Titel: Test Programm für Streufeldkompensation
+ * Cyrill Wyler 28.10.2020
  */
-
-
+//#################################################################
+//______Header Files
+#include <msp430.h> 
+//#################################################################
+//______Globale Variabeln
 unsigned char clock = 0;
 
+//#################################################################
+//______Konfigurier Funktionen
 void config_CLK_1MHZ(void)
 {
     //Select Internal 1Mhz
@@ -19,6 +22,8 @@ void config_CLK_1MHZ(void)
 }
 
 
+//#################################################################
+//______Haupt-Funktionen
 void delay_ms(int ms)
 {
     //Dynamic Delay for 1Mhz an 8Mhz same Time
@@ -30,10 +35,35 @@ void delay_ms(int ms)
 }
 
 
-char data = 0;
+void update_all_chanels()
+{
+    /*
+    Simultaneously update all channels of all DAC8564
+    X X 1 1 0 0 X X X devices in the system with data stored in each
+    channels data buffer
+    */
+    //Send 0x30
+
+    P1OUT &= (~BIT4); // Select Device
+    while(!(IFG2 & UCB0TXIFG)); // USCI_B0 TX buffer ready?
+    UCB0TXBUF = 0x30; // Send data over SPI to Slave
+    __delay_cycles(8);//NOP
+    while(!(IFG2 & UCB0TXIFG)); // USCI_B0 TX buffer ready?
+    UCB0TXBUF = 0x00; // Send data over SPI to Slave
+    __delay_cycles(8);//NOP
+    while(!(IFG2 & UCB0TXIFG)); // USCI_B0 TX buffer ready?
+    UCB0TXBUF = 0x00; // Send data over SPI to Slave
+    __delay_cycles(20);//NOP
+    P1OUT |= (BIT4); // Unselect Device
+    delay_ms(5);
+}
+
+//@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
+//______MAIN:
 int main(void)
 {
 	WDTCTL = WDTPW | WDTHOLD;	// stop watchdog timer
+
 	config_CLK_1MHZ();
 
 	//chipselect P1.4
@@ -59,21 +89,38 @@ int main(void)
 	UCB0BR1 = 0; // no prescaler
 	UCB0CTL1 &= ~UCSWRST; // **Initialize USCI state machine**
 
+    unsigned int value = 655300; //0-65536
 
-    P1OUT &= (~BIT4); // Select Device
+    //Vout = ((X/65536)*10)-5
+    float vout = -4.99;
+    while(1)
+    {
+        value = ((vout+5)/10)*65536;
 
-    while(!(IFG2 & UCB0TXIFG)); // USCI_B0 TX buffer ready?
-    UCB0TXBUF = 0x20; // Send data over SPI to Slave
-    //while(!(IFG2 & UCB0TXIFG)); // USCI_B0 TX buffer ready?
-    UCB0TXBUF = 0x20; // Send data over SPI to Slave
-    //while(!(IFG2 & UCB0TXIFG)); // USCI_B0 TX buffer ready?
-    UCB0TXBUF = 0x20; // Send data over SPI to Slave
-
-    P1OUT |= (BIT4); // Unselect Device
-
-    delay_ms(1000);
+        unsigned char Low = value & 0xFF;
+        unsigned char High = value >> 8;
 
 
+
+        P1OUT &= (~BIT4); // Select Device
+
+        while(!(IFG2 & UCB0TXIFG)); // USCI_B0 TX buffer ready?
+        UCB0TXBUF = 0x00; // Send data over SPI to Slave
+        __delay_cycles(8);//NOP
+        while(!(IFG2 & UCB0TXIFG)); // USCI_B0 TX buffer ready?
+        UCB0TXBUF = High; // Send data over SPI to Slave
+        __delay_cycles(8);//NOP
+        while(!(IFG2 & UCB0TXIFG)); // USCI_B0 TX buffer ready?
+        UCB0TXBUF = Low; // Send data over SPI to Slave
+        __delay_cycles(20);//NOP
+
+        P1OUT |= (BIT4); // Unselect Device
+
+
+        update_all_chanels();
+        delay_ms(1000);
+
+    }
     /*
     while(!(IFG2 & UCB0TXIFG)); // USCI_B0 TX buffer ready?
     UCB0TXBUF = 0xFF; // Send data over SPI to Slave
@@ -82,9 +129,6 @@ int main(void)
     data = UCB0RXBUF; // Store received data
      */
 
-    while(1);
-
-
-
-
 }
+
+
